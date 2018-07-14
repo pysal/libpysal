6ff549d3e278699a72a9126a385784ee9287df39
eli knaap <ek@knaaptime.com>
2018-07-14 17:49:16 -0500

    2to3

--- libpysal/io/IOHandlers/arcgis_swm.py
+++ libpysal/io/IOHandlers/arcgis_swm.py
@@ -56,7 +56,7 @@ class ArcGISSwmIO(FileIO.FileIO):
         self.file = open(self.dataPath, self.mode + 'b')
 
     def _set_varName(self, val):
-        if issubclass(type(val), basestring):
+        if issubclass(type(val), str):
             self._varName = val
 
     def _get_varName(self):
@@ -65,7 +65,7 @@ class ArcGISSwmIO(FileIO.FileIO):
     varName = property(fget=_get_varName, fset=_set_varName)
 
     def _set_srs(self, val):
-        if issubclass(type(val), basestring):
+        if issubclass(type(val), str):
             self._srs = val
 
     def _get_srs(self):
@@ -137,7 +137,7 @@ class ArcGISSwmIO(FileIO.FileIO):
         no_obs, row_std = tuple(unpack('<2l', self.file.read(8)))
         neighbors = {}
         weights = {}
-        for i in xrange(no_obs):
+        for i in range(no_obs):
             origin, no_nghs = tuple(unpack('<2l', self.file.read(8)))
             neighbors[origin] = []
             weights[origin] = []
@@ -176,7 +176,7 @@ class ArcGISSwmIO(FileIO.FileIO):
 
         neighbors = {}
         weights = {}
-        for i in xrange(no_obs):
+        for i in range(no_obs):
             origin, no_nghs = tuple(unpack('<2l', self.file.read(8)))
             neighbors[origin] = []
             weights[origin] = []
