580a9d1add0f6608d643c7cb72a1ffadde61ded3
ljwolf <levi.john.wolf@gmail.com>
2018-07-23 21:33:27 -0700

    update API for cg

--- libpysal/cg/__init__.py
+++ libpysal/cg/__init__.py
@@ -1,11 +1,18 @@
 """
 A module for computational geometry.
 """
-from .shapes import *
+from . import shapes
 from .standalone import *
 from .locators import *
-from .rtree import *
 from .kdtree import *
 from .sphere import *
 from .voronoi import *
 from .alpha_shapes import alpha_shape, alpha_shape_auto
+
+del rtree
+del kdtree
+del locators
+del sphere
+del voronoi
+del standalone
+del alpha_shapes
