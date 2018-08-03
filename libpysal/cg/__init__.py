ae5cee7c577426b53bee86159616f538fdb0a5cf
James Gaboardi <jgaboardi@gmail.com>
2018-08-03 16:25:58 -0400

    including rtree in imports

--- libpysal/cg/__init__.py
+++ libpysal/cg/__init__.py
@@ -5,6 +5,7 @@ from .shapes import *
 from .standalone import *
 from .locators import *
 from .kdtree import *
+from .rtree import *
 from .sphere import *
 from .voronoi import *
 from .alpha_shapes import alpha_shape, alpha_shape_auto
