3d10e579673ed9b0926d9e6b501386ea0fed5217
Serge Rey <sjsrey@gmail.com>
2019-12-06 17:10:50 -0800

    ENH: pull remote dataset in test

--- libpysal/cg/tests/test_geoJSON.py
+++ libpysal/cg/tests/test_geoJSON.py
@@ -5,13 +5,15 @@ import doctest
 import unittest
 
 
+
 class test_MultiPloygon(unittest.TestCase):
 
     def test___init__1(self):
         """
-        Tests conversion of polygons with multiple shells to 
+        Tests conversion of polygons with multiple shells to
         geoJSON multipolygons. and back.
         """
+        ncovr = pysal_examples.load_example('NCOVR')
         shp = psopen(pysal_examples.get_path("NAT.shp"),'r')
         multipolygons = [p for p in shp if len(p.parts) > 1]
         geoJSON = [p.__geo_interface__ for p in multipolygons]
