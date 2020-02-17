c90688d44cfc187850caea47b4a52b99edd0533b
James Gaboardi <jgaboardi@gmail.com>
2020-02-17 14:22:19 -0500

    adding back 3.8 to classifiers in setup.py

--- setup.py
+++ setup.py
@@ -87,7 +87,8 @@ def setup_package():
             "License :: OSI Approved :: BSD License",
             "Programming Language :: Python",
             "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7"
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8"
         ],
         package_data={"libpysal": list(example_data_files)},
         install_requires=install_reqs,
