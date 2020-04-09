fd45919460a1f6f1e75c935159c93c61fe0240ca
ljwolf <levi.john.wolf@gmail.com>
2020-04-09 11:21:35 +0100

    add pygeos processing if pygeos is available

--- libpysal/cg/alpha_shapes.py
+++ libpysal/cg/alpha_shapes.py
@@ -15,16 +15,27 @@ from ..common import requires, jit, HAS_JIT
 
 if not HAS_JIT:
     from warnings import warn
-    NUMBA_WARN = "Numba not imported, so alpha shape construction may be slower than expected."
+
+    NUMBA_WARN = (
+        "Numba not imported, so alpha shape construction may be slower than expected."
+    )
+
+try:
+    import pygeos
+
+    HAS_PYGEOS = True
+except ModuleNotFoundError:
+    HAS_PYGEOS = False
+
 
 EPS = np.finfo(float).eps
 
-__all__ = ['alpha_shape', 'alpha_shape_auto']
+__all__ = ["alpha_shape", "alpha_shape_auto"]
 
 
 @jit
 def nb_dist(x, y):
-    '''
+    """
     numba implementation of distance between points `x` and `y`
     ...
 
@@ -51,17 +62,17 @@ def nb_dist(x, y):
     >>> dist
     1.4142135623730951
 
-    '''
+    """
     sum = 0
     for x_i, y_i in zip(x, y):
-        sum += (x_i - y_i)**2
+        sum += (x_i - y_i) ** 2
     dist = np.sqrt(sum)
     return dist
 
 
 @jit(nopython=True)
 def r_circumcircle_triangle_single(a, b, c):
-    '''
+    """
     Computation of the circumcircle of a single triangle
     ...
 
@@ -97,16 +108,13 @@ def r_circumcircle_triangle_single(a, b, c):
     >>> r
     0.2500000000000001
 
-    '''
+    """
     ab = nb_dist(a, b)
     bc = nb_dist(b, c)
     ca = nb_dist(c, a)
 
     num = ab * bc * ca
-    den = np.sqrt((ab + bc + ca) *
-                  (bc + ca - ab) *
-                  (ca + ab - bc) *
-                  (ab + bc - ca))
+    den = np.sqrt((ab + bc + ca) * (bc + ca - ab) * (ca + ab - bc) * (ab + bc - ca))
     if den == 0:
         return np.array([ab, bc, ca]).max() / 2.0
     else:
@@ -115,7 +123,7 @@ def r_circumcircle_triangle_single(a, b, c):
 
 @jit(nopython=True)
 def r_circumcircle_triangle(a_s, b_s, c_s):
-    '''
+    """
     Computation of circumcircles for a series of triangles
     ...
 
@@ -144,19 +152,17 @@ def r_circumcircle_triangle(a_s, b_s, c_s):
     >>> rs = r_circumcircle_triangle(a_s, b_s, c_s)
     >>> rs
     array([3.53553391, 2.5       , 1.58113883])
-    '''
+    """
     len_a = len(a_s)
     r2 = np.zeros((len_a,))
     for i in range(len_a):
-        r2[i] = r_circumcircle_triangle_single(a_s[i],
-                                               b_s[i],
-                                               c_s[i])
+        r2[i] = r_circumcircle_triangle_single(a_s[i], b_s[i], c_s[i])
     return r2
 
 
 @jit
 def get_faces(triangle):
-    '''
+    """
     Extract faces from a single triangle
     ...
 
@@ -183,7 +189,7 @@ def get_faces(triangle):
            [1., 4.],
            [4., 3.]])
 
-    '''
+    """
     faces = np.zeros((3, 2))
     for i, (i0, i1) in enumerate([(0, 1), (1, 2), (2, 0)]):
         faces[i] = triangle[i0], triangle[i1]
@@ -191,9 +197,8 @@ def get_faces(triangle):
 
 
 @jit
-def build_faces(faces, triangles_is,
-                num_triangles, num_faces_single):
-    '''
+def build_faces(faces, triangles_is, num_triangles, num_faces_single):
+    """
     Build facing triangles
 
     ...
@@ -247,17 +252,17 @@ def build_faces(faces, triangles_is,
            [1, 0],
            [0, 2]])
 
-    '''
+    """
     for i in range(num_triangles):
         from_i = num_faces_single * i
-        to_i = num_faces_single * (i+1)
-        faces[from_i: to_i] = get_faces(triangles_is[i])
+        to_i = num_faces_single * (i + 1)
+        faces[from_i:to_i] = get_faces(triangles_is[i])
     return faces
 
 
 @jit
 def nb_mask_faces(mask, faces):
-    '''
+    """
     Run over each row in `faces`, if the face in the following row is the
     same, then mark both as False on `mask`
     ...
@@ -292,17 +297,17 @@ def nb_mask_faces(mask, faces):
            [2, 4],
            [3, 4]])
 
-    '''
-    for k in range(faces.shape[0]-1):
+    """
+    for k in range(faces.shape[0] - 1):
         if mask[k]:
-            if np.all(faces[k] == faces[k+1]):
+            if np.all(faces[k] == faces[k + 1]):
                 mask[k] = False
-                mask[k+1] = False
+                mask[k + 1] = False
     return faces[mask]
 
 
 def get_single_faces(triangles_is):
-    '''
+    """
     Extract outward facing edges from collection of triangles
     ...
 
@@ -336,15 +341,14 @@ def get_single_faces(triangles_is):
            [2, 4],
            [3, 4]])
 
-    '''
+    """
     num_faces_single = 3
     num_triangles = triangles_is.shape[0]
     num_faces = num_triangles * num_faces_single
     faces = np.zeros((num_faces, 2), dtype=np.int_)
     mask = np.ones((num_faces,), dtype=np.bool_)
 
-    faces = build_faces(faces, triangles_is,
-                        num_triangles, num_faces_single)
+    faces = build_faces(faces, triangles_is, num_triangles, num_faces_single)
 
     orderlist = ["x{}".format(i) for i in range(faces.shape[1])]
     dtype_list = [(el, faces.dtype.str) for el in orderlist]
@@ -357,9 +361,9 @@ def get_single_faces(triangles_is):
     return single_faces
 
 
-@requires('geopandas', 'shapely')
+@requires("geopandas", "shapely")
 def alpha_geoms(alpha, triangles, radii, xys):
-    '''
+    """
     Generate alpha-shape polygon(s) from `alpha` value, vertices of `triangles`,
     the `radii` for all points, and the points themselves
     ...
@@ -415,22 +419,21 @@ def alpha_geoms(alpha, triangles, radii, xys):
     0    POLYGON ((0.00000 1.00000, 3.00000 5.00000, 4....
     dtype: geometry
 
-    '''
+    """
     from shapely.geometry import LineString
     from shapely.ops import polygonize
     from geopandas import GeoSeries
 
-    triangles_reduced = triangles[radii < 1/alpha]
+    triangles_reduced = triangles[radii < 1 / alpha]
     outer_triangulation = get_single_faces(triangles_reduced)
     face_pts = xys[outer_triangulation]
-    geoms = GeoSeries(list(polygonize(list(map(LineString,
-                                               face_pts)))))
+    geoms = GeoSeries(list(polygonize(list(map(LineString, face_pts)))))
     return geoms
 
 
-@requires('geopandas', 'shapely')
+@requires("geopandas", "shapely")
 def alpha_shape(xys, alpha):
-    '''
+    """
     Alpha-shape delineation (Edelsbrunner, Kirkpatrick &
     Seidel, 1983) from a collection of points
     ...
@@ -473,14 +476,13 @@ def alpha_shape(xys, alpha):
         a set of points in the plane. IEEE Transactions on information theory,
         29(4), 551-559.
 
-    '''
+    """
     if not HAS_JIT:
         warn(NUMBA_WARN)
     if xys.shape[0] < 4:
         from shapely import ops, geometry as geom
-        return ops.cascaded_union([geom.Point(xy)
-                                   for xy in xys])\
-                  .convex_hull.buffer(0)
+
+        return ops.cascaded_union([geom.Point(xy) for xy in xys]).convex_hull.buffer(0)
     triangulation = spat.Delaunay(xys)
     triangles = xys[triangulation.simplices]
     a_pts = triangles[:, 0, :]
@@ -493,7 +495,7 @@ def alpha_shape(xys, alpha):
 
 
 def _valid_hull(geoms, points):
-    '''
+    """
     Sanity check within ``alpha_shape_auto()`` to verify the generated
     alpha shape actually contains the original set of points (xys).
 
@@ -511,22 +513,26 @@ def _valid_hull(geoms, points):
     flag    : bool
               Valid hull for alpha shape [True] or not [False]
 
-    '''
+    """
     flag = True
     # if there is not exactly one polygon
     if geoms.shape[0] != 1:
         flag = False
     # if any (xys) points do not intersect the polygon
-    for point in points:
-        if not point.intersects(geoms[0]):
-            flag = False
-    return flag
+    if HAS_PYGEOS:
+        return pygeos.intersects(pygeos.from_shapely(geoms[0]), points).all()
+    else:
+        for point in points:
+            if not point.intersects(geoms[0]):
+                return False
+        return True
 
 
-@requires('geopandas', 'shapely')
-def alpha_shape_auto(xys, step=1, verbose=False,
-                     return_radius=False, return_circles=False):
-    '''
+@requires("geopandas", "shapely")
+def alpha_shape_auto(
+    xys, step=1, verbose=False, return_radius=False, return_circles=False
+):
+    """
     Computation of alpha-shape delineation with automated selection of alpha.
     ...
 
@@ -577,17 +583,18 @@ def alpha_shape_auto(xys, step=1, verbose=False,
         a set of points in the plane. IEEE Transactions on information theory,
         29(4), 551-559.
 
-    '''
+    """
     if not HAS_JIT:
         warn(NUMBA_WARN)
     from shapely import geometry as geom
+
     if return_circles:
         return_radius = True
     if xys.shape[0] < 4:
         from shapely import ops
+
         if xys.shape[0] == 3:
-            multipoint = ops.cascaded_union([geom.Point(xy)
-                                             for xy in xys])
+            multipoint = ops.cascaded_union([geom.Point(xy) for xy in xys])
             alpha_shape = multipoint.convex_hull.buffer(0)
         else:
             alpha_shape = geom.Polygon([])
@@ -599,7 +606,7 @@ def alpha_shape_auto(xys, step=1, verbose=False,
             return alpha_shape
         elif xys.shape[0] == 2:
             if return_radius:
-                r = spat.distance.euclidean(xys[0], xys[1])/2
+                r = spat.distance.euclidean(xys[0], xys[1]) / 2
                 if return_circles:
                     circle = _construct_centers(xys[0], xys[1], r)
                     return [alpha_shape, r, circle]
@@ -623,16 +630,18 @@ def alpha_shape_auto(xys, step=1, verbose=False,
     radii_sorted_i = radii.argsort()
     triangles = triangulation.simplices[radii_sorted_i][::-1]
     radii = radii[radii_sorted_i][::-1]
-    geoms_prev = alpha_geoms((1/radii.max())-EPS, triangles, radii, xys)
-    points = [geom.Point(pnt) for pnt in xys]
+    geoms_prev = alpha_geoms((1 / radii.max()) - EPS, triangles, radii, xys)
+    if HAS_PYGEOS:
+        points = pygeos.points(xys)
+    else:
+        points = [geom.Point(pnt) for pnt in xys]
     if verbose:
-        print('Step set to %i' % step)
+        print("Step set to %i" % step)
     for i in range(0, len(radii), step):
         radi = radii[i]
         alpha = (1 / radi) - EPS
         if verbose:
-            print('%.2f%% | Trying a = %f'
-                  % ((i+1)/radii.shape[0], alpha))
+            print("%.2f%% | Trying a = %f" % ((i + 1) / radii.shape[0], alpha))
         geoms = alpha_geoms(alpha, triangles, radii, xys)
         if _valid_hull(geoms, points):
             geoms_prev = geoms
@@ -671,25 +680,25 @@ def construct_bounding_circles(alpha_shape, radius):
     coordinates = list(alpha_shape.boundary.coords)
     n_coordinates = len(coordinates)
     centers = []
-    for i in range(n_coordinates-1):
-        a, b = coordinates[i], coordinates[i+1]
+    for i in range(n_coordinates - 1):
+        a, b = coordinates[i], coordinates[i + 1]
         centers.append(_construct_centers(a, b, radius))
     return centers
 
 
 @jit(nopython=True)
 def _construct_centers(a, b, radius):
-    midpoint_x = (a[0] + b[0])*.5
-    midpoint_y = (a[1] + b[1])*.5
-    d = ((a[0] - b[0])**2 + (a[1] - b[1])**2)**.5
+    midpoint_x = (a[0] + b[0]) * 0.5
+    midpoint_y = (a[1] + b[1]) * 0.5
+    d = ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
     if b[0] - a[0] == 0:
         m = np.inf
-        axis_rotation = np.pi/2
+        axis_rotation = np.pi / 2
     else:
         m = (b[1] - a[1]) / (b[0] - a[0])
         axis_rotation = np.arctan(m)
     # altitude is perpendicular bisector of AB
-    interior_angle = np.arccos(.5 * d / radius)
+    interior_angle = np.arccos(0.5 * d / radius)
     chord = np.sin(interior_angle) * radius
 
     dx = chord * np.sin(axis_rotation)
@@ -702,27 +711,26 @@ def _construct_centers(a, b, radius):
 
     # sign gives us direction of point, since
     # shapely shapes are clockwise-defined
-    sign = np.sign((b[0]-a[0])*(up_y - a[1])
-                   - (b[1] - a[1])*(up_x - a[0]))
+    sign = np.sign((b[0] - a[0]) * (up_y - a[1]) - (b[1] - a[1]) * (up_x - a[0]))
     if sign == 1:
         return up_x, up_y
     else:
         return down_x, down_y
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     import matplotlib.pyplot as plt
     import time
     import geopandas as gpd
-    plt.close('all')
+
+    plt.close("all")
     xys = np.random.random((1000, 2))
     t0 = time.time()
     geoms = alpha_shape_auto(xys, 1)
     t1 = time.time()
-    print('%.2f Seconds to run algorithm' % (t1-t0))
+    print("%.2f Seconds to run algorithm" % (t1 - t0))
     f, ax = plt.subplots(1)
-    gpd.GeoDataFrame({'geometry': [geoms]}).plot(
-        ax=ax, color='orange', alpha=0.5)
+    gpd.GeoDataFrame({"geometry": [geoms]}).plot(ax=ax, color="orange", alpha=0.5)
     ax.scatter(xys[:, 0], xys[:, 1], s=0.1)
     plt.show()
