9a4d5f0225176dd4d414180f9ce6466764cc6aeb
Serge Rey <sjsrey@gmail.com>
2020-05-03 10:46:48 -0700

    Move coincident handling into constructor

--- libpysal/weights/distance.py
+++ libpysal/weights/distance.py
@@ -18,7 +18,48 @@ try:
 except ImportError:
     PANDAS=False
 
+def duplicated(array):
+    """Identify duplicate rows in an array
+    Parameters
+    ----------
+    array     : np.ndarray
+                (n,k)
+    Returns
+    -------
+    duplicate : np.ndarray
+                (n, 3)
+                First column indicates if the row is a duplicate
+                Second column indicates if the row is a duplicate of a row with
+                a lower index
+                Third column contains the index of the first row that
+                duplicates current row
+    Examples
+    ---------
+    >>> a = np.array([[1,1,1],[2,2,2],[3,3,3],[4,4,4],[5,5,5],[1,1,1],
+                     [2,2,2], [1,1,1]])
+    >>> duplicated(a)
+    array([[1, 0, 0],
+           [1, 0, 0],
+           [0, 0, 0],
+           [0, 0, 0],
+           [0, 0, 0],
+           [1, 1, 0],
+           [1, 1, 1],
+           [1, 1, 0]])
+    >>> duplicated(a)[:,0].any()
+    True
+    """
+    n = array.shape[0]
+    duplicate = np.zeros((n,3), dtype=int)
+    unq, count = np.unique(array, axis=0, return_counts=True)
+    repeated_groups = unq[count > 1]
+    for repeated_group in repeated_groups:
+        repeated_idx = np.argwhere(np.all(array == repeated_group, axis=1))
+        duplicate[repeated_idx, 0] = 1
+        duplicate[repeated_idx[1:], 1] = 1
+        duplicate[repeated_idx[1:], 2] = repeated_idx[0]
 
+    return duplicate
 
 def _check_duplicates(data):
     if PANDAS:
@@ -114,21 +155,47 @@ class KNN(W):
         else:
             self.kdtree = KDTree(data, radius=radius, distance_metric=distance_metric)
             self.data = self.kdtree.data
+
+        duplicates = duplicated(self.data)
+        coincident = duplicates[:,1].any()
+
+        self.duplicates = duplicates
         self.k = k 
         self.p = p
-        this_nnq = self.kdtree.query(self.data, k=k+1, p=p)
+        data = self.data
+        if coincident:
+            duplicate_ids = np.nonzero(duplicates[:,1])
+            data = self.data[np.nonzero(duplicates[:,1]==0)]
+            self.kdtree = KDTree(data, radius=radius, distance_metric=distance_metric)
+
+        this_nnq = self.kdtree.query(data, k=k+1, p=p)
 
         to_weight = this_nnq[1]
         if ids is None:
             ids = list(range(to_weight.shape[0]))
 
         neighbors = {}
-        for i,row in enumerate(to_weight):
-            row = row.tolist()
-            row.remove(i)
-            row = [ids[j] for j in row]
-            focal = ids[i]
-            neighbors[focal] = row
+        if coincident:
+            unique_ids = np.nonzero(duplicates[:,1]==0)[0]
+            print(unique_ids)
+            for i, row in enumerate(to_weight):
+                row = row.tolist()
+                row.remove(i)
+                row = [unique_ids[j] for j in row]
+                focal = unique_ids[i]
+                neighbors[focal] = row
+            print(neighbors)
+            for row in duplicate_ids[0]:
+                neighbors[row] = neighbors[duplicates[row, 2]]
+            n = self.data.shape[0]
+            ids = list(range(n))
+        else:
+           for i,row in enumerate(to_weight):
+               row = row.tolist()
+               row.remove(i)
+               row = [ids[j] for j in row]
+               focal = ids[i]
+               neighbors[focal] = row
         W.__init__(self, neighbors, id_order=ids, **kwargs)
 
     @classmethod
@@ -299,10 +366,8 @@ class KNN(W):
                 match = value['index']
                 neighbors[index] = neighbors[match]
             w = W(neighbors=neighbors)
-            w.k = k
-            w.p = p
             return w
-        return cls(pts, *args, ids=ids, coincident=coincident, **kwargs)
+        return cls(pts, *args, ids=ids, **kwargs)
 
     def reweight(self, k=None, p=None, new_data=None, new_ids=None, inplace=True):
         """
