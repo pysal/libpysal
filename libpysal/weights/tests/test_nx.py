d8f41ec96fadf56b7cedaed3ca14bf1e5fd90990
mjschwenne <mjschwenne@gmail.com>
2022-06-15 16:55:03 -0500

    update nx conversions to be 3.0 compatible

--- libpysal/weights/tests/test_nx.py
+++ libpysal/weights/tests/test_nx.py
@@ -13,17 +13,17 @@ except ImportError:
 class Test_NetworkXConverter(ut.TestCase):
     def setUp(self):
         self.known_nx = nx.random_regular_graph(4, 10, seed=8879)
-        self.known_amat = np.array(nx.to_numpy_matrix(self.known_nx))
+        self.known_amat = nx.to_numpy_array(self.known_nx)
         self.known_W = lat2W(5, 5)
 
     def test_round_trip(self):
         W_ = W.from_networkx(self.known_nx)
         np.testing.assert_allclose(W_.sparse.toarray(), self.known_amat)
         nx2 = W_.to_networkx()
-        np.testing.assert_allclose(nx.to_numpy_matrix(nx2), self.known_amat)
+        np.testing.assert_allclose(nx.to_numpy_array(nx2), self.known_amat)
         nxsquare = self.known_W.to_networkx()
         np.testing.assert_allclose(
-            self.known_W.sparse.toarray(), nx.to_numpy_matrix(nxsquare)
+            self.known_W.sparse.toarray(), nx.to_numpy_array(nxsquare)
         )
         W_square = W.from_networkx(nxsquare)
         np.testing.assert_allclose(
