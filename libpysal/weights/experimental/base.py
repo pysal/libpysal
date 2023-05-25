8e4b8e34b2e00b28ab515d9c14f1d78109d810a0
ljwolf <levi.john.wolf@gmail.com>
2023-05-25 10:55:48 -0700

    consolidate on simple wdf

--- libpysal/weights/experimental/base.py
+++ libpysal/weights/experimental/base.py
@@ -1,37 +1,36 @@
-from xarray import DataArray
 from scipy import sparse
 import numpy
-from sklearn.preprocessing import LabelEncoder
 
 
-class W_XA:
+class Graph:
     def __init__(self, adjlist):
-        self.xarray = DataArray.from_series(
-            adjlist.set_index(["focal", "neighbor"])["weight"], sparse=True
-        ).fillna(0)
+        self.adjlist = adjlist
         self._cache = dict()
 
     @classmethod
     def from_w(cls, w):
-        return cls(w.to_adjlist())
+        return cls(w.to_adlist())
 
     def neighbors(self, ix):
         # assume that the ix is not iterable, like currently done in W
-        (neighbors,) = self.xarray.sel(focal=ix).data.nonzero()
-        return self.xarray.neighbor[neighbors]
+        return self.adjlist[self.adjlist.focal == ix].neighbor
 
     def weights(self, ix):
         # assume that the ix is not iterable, like currently done in W
-        neighbors = self.neighbors(ix)
-        return self.xarray.sel(focal=ix).data[neighbors]
+        return self.adjlist[self.adjlist.focal == ix].weight
 
     @property
     def sparse(self):
         try:
             return self._cache["sparse"]
         except KeyError:
-            coo = self.xarray.data
-            self._cache["sparse"] = sparse.csr_matrix((coo.data, coo.nonzero()))
+            weights = self.adjlist.weight
+            idxs = numpy.unique(
+                numpy.hstack((self.adjlist.focal.values, self.adjlist.neighbor.values))
+            )
+            rows = numpy.searchsorted(idxs, self.adjlist.focal)
+            cols = numpy.searchsorted(idxs, self.adjlist.neighbor)
+            self._cache["sparse"] = sparse.csr_matrix((weights, (rows, cols)))
             return self.sparse
 
 
@@ -41,4 +40,4 @@ if __name__ == "__main__":
     counties = geopandas.read_file(libpysal.examples.get_path("NAT.shp"))
     w = libpysal.weights.Rook.from_dataframe(counties, ids=counties.FIPS.tolist())
     alist = w.to_adjlist()
-    wx = W_XA(alist)
+    wd = Graph(alist)
