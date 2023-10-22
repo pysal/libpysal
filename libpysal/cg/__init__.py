9f2dcefffa54326ab10bea38cc36715153652440
James Gaboardi <jgaboardi@gmail.com>
2023-10-22 19:13:46 -0400

    lint 2nd-level __init__ files

--- libpysal/cg/__init__.py
+++ libpysal/cg/__init__.py
@@ -1,11 +1,11 @@
 """
 A module for computational geometry.
 """
-from .shapes import *
-from .standalone import *
-from .locators import *
-from .kdtree import *
-from .rtree import *
-from .sphere import *
-from .voronoi import *
 from .alpha_shapes import *
+from .kdtree import *
+from .locators import *
+from .rtree import *
+from .shapes import *
+from .sphere import *
+from .standalone import *
+from .voronoi import *
