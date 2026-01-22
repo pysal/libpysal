f38960ace9efb86d4292a2fe11d55a79fb8126f8
Martin Fleischmann <martin@martinfleischmann.net>
2026-01-22 21:18:10 +0100

    COMPAT: pandas 3 compat in kernel

--- libpysal/kernels.py
+++ libpysal/kernels.py
@@ -297,8 +297,12 @@ def kernel(distances, bandwidth, kernel="gaussian", taper=True, decay=False):
 
     k = func(distances, bandwidth)
     if taper is True:
+        # pandas 3.0 Copy-on-Write consequence
+        k = k.copy()
         k[distances > bandwidth] = 0.0
     elif isinstance(taper, (float, int)) and not isinstance(taper, bool):
+        # pandas 3.0 Copy-on-Write consequence
+        k = k.copy()
         k[distances > taper] = 0.0
 
     if decay:
